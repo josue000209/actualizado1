38c5ca679ede9bd459c7c42fa15df0480816caeb
josue <josue.tamayo@cbtis72.edu.mx>
2019-03-12 22:22:28 -0500

    xd1

--- app/src/main/java/com/alejandrolora/seccion_04_realm_lab/Intro.java
+++ app/src/main/java/com/alejandrolora/seccion_04_realm_lab/Intro.java
@@ -11,13 +11,15 @@ import com.alejandrolora.seccion_04_realm_lab.activities.CityActivity;
 import com.alejandrolora.seccion_04_realm_lab.models.City;
 
 public class Intro extends AppCompatActivity {
-
+    private PrefManager prefManager;
 
     private View btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        prefManager = new PrefManager(this);
+
         setContentView(R.layout.activity_intro);
 //codigo para pasar de un layout a otra
         btn = (Button) findViewById(R.id.buttonMain);
